d11f83d562f85b9a8470fcf66f860daf10b18316
frederic devernay <Frederic.Devernay@inria.fr>
2013-08-29 13:09:48 +0200

    added Makefiles inspires by the ones in Support/Plugins

--- Support/Library/ofxsLog.cpp
+++ Support/Library/ofxsLog.cpp
@@ -46,8 +46,6 @@ The log file is written to using printf style functions, rather than via c++ ios
 #include <cstdlib>
 #include <string>
 
-#include "ofxsLog.h"
-
 namespace OFX {
   namespace Log {
 

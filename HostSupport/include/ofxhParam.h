ff152cccc58f65b3d13b958827a01b82876eb04c
MrKepzie <alexandre230@msn.com>
2013-12-17 10:48:29 +0100

    Hide implementation details into ofxhParam.cpp so the host doesn’t have to be aware of the existence of the static member

--- HostSupport/include/ofxhParam.h
+++ HostSupport/include/ofxhParam.h
@@ -50,49 +50,14 @@ namespace OFX {
     namespace Param {
 
       /// fetch the param suite
-      const void *GetSuite(int version);
-
-      bool isColourParam(const std::string &paramType);
-
-      bool isIntParam(const std::string &paramType);
-
-      class ParamsTypeRegistery
-      {
-          struct Type {
-              Property::TypeEnum propType;
-              int propDimension;
-              
-              Type(Property::TypeEnum type,int dimension)
-              : propType(type)
-              , propDimension(dimension)
-              {}
-          };
-          
-      public:
-          
-          ParamsTypeRegistery();
-          
-          bool isDoubleParam(const std::string &paramType);
-          
-          bool isColourParam(const std::string &paramType);
-          
-          bool isIntParam(const std::string &paramType);
-          
-          /// allows to add more types than the built-in ones (e.g parametric params)
-          void addStandardType(const std::string& typeName,Property::TypeEnum type,int dimension);
-          
-          /// is this a standard type
-          bool isStandardType(const std::string &type);
-          
-          bool findType(const std::string paramType, Property::TypeEnum &propType, int &propDim);
-          
-      private:
-          
-          std::map<std::string,Type> _typesMap;
-      };
+      void *GetSuite(int version);
         
-     static ParamsTypeRegistery globalParamsTypeRegistery = ParamsTypeRegistery();
-      
+      /// allows to add more types than the built-in ones (e.g parametric params)
+      void addStandardType(const std::string& typeName,Property::TypeEnum type,int dimension);
+        
+      /// is this a standard type
+      bool isStandardType(const std::string &type);
+        
       /// base class for all params
       class Base {
 

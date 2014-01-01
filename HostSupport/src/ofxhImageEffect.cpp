3753aca7bca1f3ada1fc1a9eb3bdd1adb9be8407
MrKepzie <alexandre230@msn.com>
2014-01-01 09:40:32 +0100

    Fix conditions in multi-thread suite

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -2580,18 +2580,20 @@ namespace OFX {
         
         static OfxStatus mutexDestroy(const OfxMutexHandle mutex)
         {
-            if (mutex == 0)
-                return kOfxStatErrBadHandle;
+            
             if(gImageEffectHost->implementsMultiThreadSuite()){
+                if (mutex == 0)
+                    return kOfxStatErrBadHandle;
                 gImageEffectHost->mutexDestroy(mutex);
             }
             return kOfxStatOK;
         }
         
         static OfxStatus mutexLock(const OfxMutexHandle mutex){
-            if (mutex == 0)
-                return kOfxStatErrBadHandle;
+            
             if(gImageEffectHost->implementsMultiThreadSuite()){
+                if (mutex == 0)
+                    return kOfxStatErrBadHandle;
                 gImageEffectHost->mutexLock(mutex);
             }
             
@@ -2599,18 +2601,20 @@ namespace OFX {
         }
         
         static OfxStatus mutexUnLock(const OfxMutexHandle mutex){
-            if (mutex == 0)
-                return kOfxStatErrBadHandle;
+            
             if(gImageEffectHost->implementsMultiThreadSuite()){
+                if (mutex == 0)
+                    return kOfxStatErrBadHandle;
                 gImageEffectHost->mutexUnLock(mutex);
             }
             return kOfxStatOK;
         }
         
         static OfxStatus mutexTryLock(const OfxMutexHandle mutex){
-            if (mutex == 0)
-                return kOfxStatErrBadHandle;
+            
             if(gImageEffectHost->implementsMultiThreadSuite()){
+                if (mutex == 0)
+                    return kOfxStatErrBadHandle;
                 return gImageEffectHost->mutexTryLock(mutex);
             }
             return kOfxStatOK;

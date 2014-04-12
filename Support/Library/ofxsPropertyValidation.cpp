26b7dfb3885df50db26099c90e9c017e585dc728
Frédéric Devernay <frederic.devernay@m4x.org>
2014-04-12 22:25:24 +0200

    Support (ofxsPropertyValidation): kOfxImagePropPixelAspectRatio is not in the outargs of GetClipPreferences, but kOfxImageClipPropFieldOrder is

--- Support/Library/ofxsPropertyValidation.cpp
+++ Support/Library/ofxsPropertyValidation.cpp
@@ -689,9 +689,9 @@ namespace OFX {
     {
       PropertyDescription(kOfxImageEffectPropFrameRate,         OFX::eDouble, 1, eDescFinished),
       PropertyDescription(kOfxImageClipPropFieldOrder,          OFX::eString, 1, eDescFinished),
+      PropertyDescription(kOfxImageEffectPropPreMultiplication, OFX::eString, 1, eDescFinished),
       PropertyDescription(kOfxImageClipPropContinuousSamples,   OFX::eInt, 1, eDescDefault, 0, eDescFinished),
       PropertyDescription(kOfxImageEffectFrameVarying,          OFX::eInt, 1, eDescDefault, 0, eDescFinished),
-      PropertyDescription(kOfxImageEffectPropPreMultiplication, OFX::eString, 1, eDescFinished),
     };
 
     /** @brief kOfxImageEffectActionGetClipPreferences property set */

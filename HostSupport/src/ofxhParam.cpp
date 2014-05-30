325231a05212421bab3df765173596fb63c6f62d
Frédéric Devernay <frederic.devernay@m4x.org>
2014-05-30 16:17:50 +0200

    HostSupport: Instance::copy -> copyFrom, and there is no need fo two entry points

--- HostSupport/src/ofxhParam.cpp
+++ HostSupport/src/ofxhParam.cpp
@@ -680,13 +680,8 @@ namespace OFX {
         }
       }
 
-      // copy one parameter to another
-      OfxStatus Instance::copy(const Instance &/*instance*/, OfxTime /*offset*/) {
-        return kOfxStatErrMissingHostFeature; 
-      }
-
-      // copy one parameter to another, with a range
-      OfxStatus Instance::copy(const Instance &/*instance*/, OfxTime /*offset*/, OfxRangeD /*range*/) {
+      // copy one parameter to another, with a range (NULL means to copy all animation)
+      OfxStatus Instance::copyFrom(const Instance &/*instance*/, OfxTime /*offset*/, const OfxRangeD* /*range*/) {
         return kOfxStatErrMissingHostFeature; 
       }
 
@@ -2260,12 +2255,7 @@ namespace OFX {
           return kOfxStatErrBadHandle;
         }
 
-        OfxStatus stat;
-        if(!frameRange) {
-          stat = paramInstanceTo->copy(*paramInstanceFrom,dstOffset);
-        } else {
-          stat = paramInstanceTo->copy(*paramInstanceFrom,dstOffset,*frameRange);
-        }
+        OfxStatus stat = paramInstanceTo->copyFrom(*paramInstanceFrom,dstOffset,frameRange);
 #       ifdef OFX_DEBUG_PARAMETERS
         std::cout << ' ' << StatStr(stat) << std::endl;
 #       endif

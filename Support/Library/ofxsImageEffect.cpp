ca3c25b4637ac4db0e98040fee79be126cdc149c
Frédéric Devernay <frederic.devernay@m4x.org>
2014-06-29 05:06:23 +0200

    don’t throw if OFX 1.2 props are not present

--- Support/Library/ofxsImageEffect.cpp
+++ Support/Library/ofxsImageEffect.cpp
@@ -2429,8 +2429,10 @@ namespace OFX {
       args.openGLEnabled = inArgs.propGetInt(kOfxImageEffectPropOpenGLEnabled, false) != 0;
 #endif
       args.isInteractive = inArgs.propGetInt(kOfxPropIsInteractive) != 0;
-      args.sequentialRenderStatus = inArgs.propGetInt(kOfxImageEffectPropSequentialRenderStatus) != 0;
-      args.interactiveRenderStatus = inArgs.propGetInt(kOfxImageEffectPropInteractiveRenderStatus) != 0;
+      // Don't throw an exception if the following inArgs are not present:
+      // They appeared in OFX 1.2
+      args.sequentialRenderStatus = inArgs.propGetInt(kOfxImageEffectPropSequentialRenderStatus, false) != 0;
+      args.interactiveRenderStatus = inArgs.propGetInt(kOfxImageEffectPropInteractiveRenderStatus, false) != 0;
 
       // and call the plugin client render code
       effectInstance->beginSequenceRender(args);
@@ -2454,8 +2456,10 @@ namespace OFX {
       args.openGLEnabled = inArgs.propGetInt(kOfxImageEffectPropOpenGLEnabled, false) != 0;
 #endif
       args.isInteractive = inArgs.propGetInt(kOfxPropIsInteractive) != 0;
-      args.sequentialRenderStatus = inArgs.propGetInt(kOfxImageEffectPropSequentialRenderStatus) != 0;
-      args.interactiveRenderStatus = inArgs.propGetInt(kOfxImageEffectPropInteractiveRenderStatus) != 0;
+      // Don't throw an exception if the following inArgs are not present:
+      // They appeared in OFX 1.2
+      args.sequentialRenderStatus = inArgs.propGetInt(kOfxImageEffectPropSequentialRenderStatus, false) != 0;
+      args.interactiveRenderStatus = inArgs.propGetInt(kOfxImageEffectPropInteractiveRenderStatus, false) != 0;
 
       // and call the plugin client render code
       effectInstance->endSequenceRender(args);

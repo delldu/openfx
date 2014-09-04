e9e579cb60d9eeb871f21801d7beba398fa080c6
Frédéric Devernay <frederic.devernay@m4x.org>
2014-09-04 16:09:19 +0200

    add kOfxParamFileName definition

--- include/ofxImageEffect.h
+++ include/ofxImageEffect.h
@@ -99,7 +99,7 @@ typedef struct OfxImageMemoryStruct *OfxImageMemoryHandle;
 #ifdef OFX_EXTENSIONS_TUTTLE
 /** @brief Use to define the reader effect context  See \ref ::kOfxImageEffectPropContext
  An image effect that supports this context must have a string parameter
- with script name (@see kOfxParamPropScriptName) "filename" (@see kOfxParamPropScriptName),
+ with script name (@see kOfxParamPropScriptName) kOfxParamFileName,
  and the image effect descriptor may have the property kTuttleOfxImageEffectPropSupportedExtensions
  ("TuttleOfxImageEffectPropSupportedExtensions"), which is an
  n-dimensional string. */
@@ -107,11 +107,14 @@ typedef struct OfxImageMemoryStruct *OfxImageMemoryHandle;
 
 /** @brief Use to define the writer effect context  See \ref ::kOfxImageEffectPropContext
  An image effect that supports this context must have a string parameter
- with script name (@see kOfxParamPropScriptName) "filename" (@see kOfxParamPropScriptName),
+ with script name (@see kOfxParamPropScriptName) kOfxParamFileName,
  and the image effect descriptor may have the property kTuttleOfxImageEffectPropSupportedExtensions
  ("TuttleOfxImageEffectPropSupportedExtensions"), which is an
  n-dimensional string. */
 #define kOfxImageEffectContextWriter "OfxImageEffectContextWriter"
+
+/** @brief The parameter name to use in reader and writer contexts for the file name. */
+#define kOfxParamFileName "filename"
 #endif
 
 /** @brief Used as a value for ::kOfxPropType on image effect host handles */

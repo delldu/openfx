798f9a1a08874be70d0e2b8a2060393591db2340
Frédéric Devernay <frederic.devernay@m4x.org>
2014-09-19 14:09:22 +0200

    clipGetRegionOfDefinition: dont return kOfxStatFailed if the RoD is empty
    found while working on https://github.com/MrKepzie/Natron/issues/223

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -2276,8 +2276,8 @@ namespace OFX {
         }
 
         *bounds = clipInstance->getRegionOfDefinition(time);
-        if (bounds->x2 <= bounds->x1 || bounds->y2 <= bounds->y1) {
-          // the RoD is empty
+        if (bounds->x2 < bounds->x1 || bounds->y2 < bounds->y1) {
+          // the RoD is invalid (empty is OK)
 
           return kOfxStatFailed;
         }

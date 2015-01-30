f5e32ea67dccec96cd8fe0b57fcec38d268c777c
Frederic Devernay <frederic.devernay@inria.fr>
2015-01-30 12:17:22 +0100

    fix warning

--- Support/Library/ofxsImageEffect.cpp
+++ Support/Library/ofxsImageEffect.cpp
@@ -290,6 +290,7 @@ namespace OFX {
       case eBitDepthCustom:
         return std::string();
         }
+      return std::string();
     }
 
   /** @brief turns a pixel component string into and enum */

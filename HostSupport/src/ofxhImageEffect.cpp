2e24aa86d80a528643e309258b1c0375a741ddea
Frédéric Devernay <frederic.devernay@m4x.org>
2015-02-24 11:11:38 +0100

    restore indent

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -1954,8 +1954,7 @@ namespace OFX {
             ++it) {
           ClipInstance *clip = it->second;
 
-          if (!clip->isOutput()) {
-              
+          if(!clip->isOutput()) {
             bool connected = clip->getConnected();
              
             if (connected) {
@@ -1968,18 +1967,18 @@ namespace OFX {
             const std::string &rawDepth = clip->getUnmappedBitDepth();
             const std::string &rawPreMult = clip->getPremult();            
               
-            if (isChromaticComponent(rawComp)) {
-              if (connected) {
-                if (rawPreMult == kOfxImagePreMultiplied)
+            if(isChromaticComponent(rawComp)) {
+              if(connected) {
+                if(rawPreMult == kOfxImagePreMultiplied)
                   premult = kOfxImagePreMultiplied;
-                else if (rawPreMult == kOfxImageUnPreMultiplied && premult != kOfxImagePreMultiplied)
+                else if(rawPreMult == kOfxImageUnPreMultiplied && premult != kOfxImagePreMultiplied)
                   premult = kOfxImageUnPreMultiplied;
               }
                 
-              if (!clip->isOptional()) {
-                  hasSetCompsAndDepth = true;
-                  deepestBitDepth = FindDeepestBitDepth(deepestBitDepth, rawDepth);
-                  mostComponents  = findMostChromaticComponents(mostComponents, rawComp);
+              if(!clip->isOptional()) {
+                hasSetCompsAndDepth = true;
+                deepestBitDepth = FindDeepestBitDepth(deepestBitDepth, rawDepth);
+                mostComponents  = findMostChromaticComponents(mostComponents, rawComp);
               }
             }
           }

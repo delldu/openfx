f74d02cf9ca5fd519a8c045d16b31e3b5cf5a313
Frédéric Devernay <frederic.devernay@m4x.org>
2015-09-02 13:54:55 +0200

    better support for kNatronOFXImageEffectPropDeprecated (host+plugins)

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -127,6 +127,7 @@ namespace OFX {
         { kNatronOfxImageEffectPropChannelSelector, Property::eString, 1, true, kOfxImageComponentRGBA },
         { kNatronOfxImageEffectPropHostMasking, Property::eInt, 1, true, "0" },
         { kNatronOfxImageEffectPropHostMixing, Property::eInt, 1, true, "0" },
+        { kNatronOFXImageEffectPropDeprecated, Property::eInt, 1, true, "0" },
 #endif
         Property::propSpecEnd
       };
@@ -334,6 +335,13 @@ namespace OFX {
       }
 #endif
 
+#ifdef OFX_EXTENSIONS_NATRON
+      /// does this effect handle transform effects
+      bool Base::isDeprecated() const
+      {
+        return _properties.getIntProperty(kNatronOFXImageEffectPropDeprecated) != 0;
+      }
+#endif
       ////////////////////////////////////////////////////////////////////////////////
       // descriptor
 

64649baacbeed64ac353ead764f6122e79953909
Frederic Devernay <frederic.devernay@m4x.org>
2016-07-22 16:32:09 +0200

    spelling: persistant->persitent, persistance->persistence (except for the OFX strings, which keeps the spelling error)

--- Support/include/ofxsParam.h
+++ Support/include/ofxsParam.h
@@ -376,8 +376,8 @@ namespace OFX {
         /** @brief set whether the param can animate, defaults to true in most cases */
         void setAnimates(bool v);
 
-        /** @brief set whether the param is persistant, defaults to true */
-        void setIsPersistant(bool v);
+        /** @brief set whether the param is persistent, defaults to true */
+        void setIsPersistent(bool v);
 
         /** @brief Set's whether the value of the param is significant (ie: affects the rendered image), defaults to true */
         void setEvaluateOnChange(bool v);
@@ -1238,7 +1238,7 @@ namespace OFX {
         bool getIsAutoKeying(void) const;
     
         /** @brief is the param animating */
-        bool getIsPersistant(void) const;
+        bool getIsPersistent(void) const;
     
         /** @brief Get's whether the value of the param is significant (ie: affects the rendered image) */
         bool getEvaluateOnChange(void) const;

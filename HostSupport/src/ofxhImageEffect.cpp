01a7d89ce6e3dea4ce8065d75b11bb8b52f5e15f
Frédéric Devernay <frederic.devernay@m4x.org>
2019-06-29 13:59:11 -0700

    HostSupport: fix warning

--- HostSupport/src/ofxhImageEffect.cpp
+++ HostSupport/src/ofxhImageEffect.cpp
@@ -3447,7 +3447,7 @@ namespace OFX {
           *memoryHandle = NULL;
           return kOfxStatErrMemory;
         }
-        } catch (std::bad_alloc) {
+        } catch (std::bad_alloc&) {
           *memoryHandle = NULL;
           return kOfxStatErrMemory;
         } catch (...) {
